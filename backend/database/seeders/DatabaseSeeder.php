ac41f7301034e039e63aae6b8516f02f5abd3fd5
K <147337079+Anonymouseer@users.noreply.github.com>
2026-08-15 00:47:05 +0800

    Add DeploymentSeeder and register in DatabaseSeeder for PostgreSQL

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             JobOrderSeeder::class,
             ApplicantSeeder::class,
             ClientAccountSeeder::class,
+            DeploymentSeeder::class,
         ]);
     }
 }
